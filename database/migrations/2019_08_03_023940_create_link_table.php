33fe50610dc98918b2513047753962d68bf5e6e1
Ronaldo Cristover <ronaldochristover@gmail.com>
2019-08-03 09:47:25 +0700

    Add Migration and fix readme

---
+++ database/migrations/2019_08_03_023940_create_link_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLinkTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('link', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('url');
+            $table->text('product_name');
+            $table->text('product_desc');
+            $table->text('product_price');
+            $table->text('product_image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('link');
+    }
+}
